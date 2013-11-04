652f09c6e6375abe2d28e47c654bdcfebe827828
Marc Remolt <marc.remolt@googlemail.com>
2013-11-04 22:46:11 +0100

    Add neobundle to install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ ln -sfn ~/.dotfiles/zshrc ~/.zshrc
 ln -sfn ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -sfn ~/.dotfiles/vimrc ~/.vimrc
 ln -sfn ~/.dotfiles/vim ~/.vim
+
+# Neobundle
+git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+vim -c ':NeoBundleInstall|q'
